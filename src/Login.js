6c471f5ae3f40ba258c16d6cfdbe0e3128f8e75b
Peter Rai <rai.peter2@gmail.com>
2020-11-22 14:05:13 +0530

    first commit

---
+++ src/Login.js
@@ -0,0 +1,15 @@
+  import React from 'react'
+
+function Login() {
+    return (
+        <div className="login">
+            <img 
+                src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" 
+                alt="" 
+            />
+            <a href="">LOGIN WITH SPOTIFY</a>
+        </div>
+    )
+}
+
+export default Login
